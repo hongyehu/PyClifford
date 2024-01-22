ea4684747efdeb281559d3f9d884992249e21591
Hong-Ye Hu <50563225+hongyehu@users.noreply.github.com>
2024-01-22 14:01:20 -0500

    Update setup.py
    temporarily fix the double egg-info issue

--- setup.py
+++ setup.py
@@ -40,22 +40,22 @@ config = {
 }
 
 
-config2 = {
-    'name': 'torchclifford',
-    'packages': find_packages(exclude=['doc']),
-    'description': 'Clifford Gate Circuit Simulation',
-    'long_description': README,
-    'long_description_content_type' : 'text/x-rst',
-    'author': 'author names', #'version': VERSION,
-    'install_requires': ['torch'],
-    'license': 'Modified BSD',
-    'scripts': [],
-    'classifiers': [
-        'Topic :: Scientific/Engineering',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3'
-    ],
-}
+# config2 = {
+#     'name': 'torchclifford',
+#     'packages': find_packages(exclude=['doc']),
+#     'description': 'Clifford Gate Circuit Simulation',
+#     'long_description': README,
+#     'long_description_content_type' : 'text/x-rst',
+#     'author': 'author names', #'version': VERSION,
+#     'install_requires': ['torch'],
+#     'license': 'Modified BSD',
+#     'scripts': [],
+#     'classifiers': [
+#         'Topic :: Scientific/Engineering',
+#         'License :: OSI Approved :: BSD License',
+#         'Programming Language :: Python :: 3'
+#     ],
+# }
 
 setup(**config)
-setup(**config2)
+# setup(**config2)
